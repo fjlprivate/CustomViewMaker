3cdbe0f5ab973e9487394982f7bce5a0b7bed39b
fjlprivate <fjlprivate88@126.com>
2016-04-29 09:43:40 +0800

    V1.0.1-wifiView-开始添加animation;

--- CustomViewMaker/WifiView/WifiView.m
+++ CustomViewMaker/WifiView/WifiView.m
@@ -34,13 +34,13 @@
 }
 - (void) initial {
     self.hiddenWhenAnimationEnd = YES;
-    [self.layer addSublayer:self.shapeLayer];
+    [self.layer addSublayer:self.ringCircleLayer];
+    [self.layer addSublayer:self.centerCircleLayer];
 }
 
 - (void)layoutSubviews {
     [super layoutSubviews];
     
-    /* remake bezierPath to shapeLayer by animationCount */
     CGRect frame = self.bounds;
     CGFloat angle = frame.size.width * 0.5 * 0.25 * sqrt(2);
     
@@ -49,35 +49,64 @@
     
     CGPoint centerBottomP = CGPointMake(centerX, bottomY);
     
+    /* remake bezierPath to centerCircleLayer  */
     UIBezierPath* path = [UIBezierPath bezierPath];
-    
+    [path moveToPoint:centerBottomP];
     [path addArcWithCenter:centerBottomP radius:angle startAngle:M_PI * 1.25 endAngle:M_PI * 1.75 clockwise:YES];
+    self.centerCircleLayer.path = path.CGPath;
     
+    
+    /* remake bezierPath to ringCircleLayer by animationCount */
+    UIBezierPath* ringPath = [UIBezierPath bezierPath];
     for (int i = 0; i < self.animationCount; i++) {
         UIBezierPath* inPath = [UIBezierPath bezierPathWithArcCenter:centerBottomP radius:(i + 2) * angle startAngle:M_PI * 1.25 endAngle:M_PI * 1.75 clockwise:YES];
-        [path appendPath:inPath];
+        [ringPath appendPath:inPath];
     }
-    
-    self.shapeLayer.path = path.CGPath;
+    self.ringCircleLayer.path = ringPath.CGPath;
 }
 
 
 - (void) startAnimationOnFinished:(void (^) (void))finishedBlock {
-    
+    [NSTimer scheduledTimerWithTimeInterval:0.3 + 0.2 + 0.1 target:self selector:@selector(doAnimation) userInfo:nil repeats:YES];
 }
 
 - (void) stopAnimationOnFinished:(void (^) (void))finishedBlock {
     
 }
 
+- (void) doAnimation {
+    __weak typeof(self) wself = self;
+    [UIView animateWithDuration:0 delay:0.3 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        wself.animationCount = 0;
+        [wself setNeedsLayout];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0 delay:0.2 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            wself.animationCount = 1;
+            [wself setNeedsLayout];
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:0 delay:0.1 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+                wself.animationCount = 2;
+                [wself setNeedsLayout];
+            } completion:^(BOOL finished) {
+                
+            }];
+        }];
+    }];
+}
 
 
 # pragma mask 4 getter
-- (CAShapeLayer *)shapeLayer {
-    if (!_shapeLayer) {
-        _shapeLayer = [CAShapeLayer layer];
+- (CAShapeLayer *)ringCircleLayer {
+    if (!_ringCircleLayer) {
+        _ringCircleLayer = [CAShapeLayer layer];
     }
-    return _shapeLayer;
+    return _ringCircleLayer;
+}
+- (CAShapeLayer *)centerCircleLayer {
+    if (!_centerCircleLayer) {
+        _centerCircleLayer = [CAShapeLayer layer];
+    }
+    return _centerCircleLayer;
 }
 
 
